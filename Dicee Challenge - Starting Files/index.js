57ecf932972498fbeff11838a21704aea7890bd9
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-05-12 17:45:19 +0200

    Change the left die betweeen 1 - 6 using the random number generator

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,4 +1,4 @@
 let randomNumber1 = Math.random();
 randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
 
-console.log(randomNumber1);
+document.querySelector("img").setAttribute("src","./images/dice"+randomNumber1+".png");
